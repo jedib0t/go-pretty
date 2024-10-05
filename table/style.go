af8f48d68220d26fcc3867572601e13fdcb20049
Naveen Mahalingam <jedib0t@outlook.com>
2024-10-05 09:58:01 -0700

    fix doc

--- table/style.go
+++ table/style.go
@@ -821,9 +821,13 @@ var (
 
 // SizeOptions defines the way to control the width of the table output.
 type SizeOptions struct {
-	// WidthMax is the maximum allotted width for the full row
+	// WidthMax is the maximum allotted width for the full row;
+	// any content beyond this will be truncated using the text
+	// in Style.Box.UnfinishedRow
 	WidthMax int
-	// WidthMin is the minimum allotted width for the full row
+	// WidthMin is the minimum allotted width for the full row;
+	// columns will be auto-expanded until the overall width
+	// is met
 	WidthMin int
 }
 
