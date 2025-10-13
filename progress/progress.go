09663b3b1c33ed709cc59e8044b9cee191c8550e
zplin <zplin@uber.com>
2025-10-13 23:46:17 +0000

    fixing tests

--- progress/progress.go
+++ progress/progress.go
@@ -346,14 +346,13 @@ func (p *Progress) initForRender() {
 	// if not output write has been set, output to STDOUT
 	if p.outputWriter == nil {
 		p.outputWriter = os.Stdout
-		// pick a sane update frequency if none set
-		if p.updateFrequency <= 0 {
-			p.updateFrequency = DefaultUpdateFrequency
-		}
-
 		// get the current terminal size for preventing roll-overs, and do this in a
 		// background loop until end of render
 		go p.watchTerminalSize() // needs p.updateFrequency
+	}
+	// pick a sane update frequency if none set
+	if p.updateFrequency <= 0 {
+		p.updateFrequency = DefaultUpdateFrequency
 	}
 
 }
